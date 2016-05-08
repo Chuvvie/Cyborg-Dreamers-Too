bcd13ddd9dc3320c2fd62a3ce4bcf6ad46390c1e
erocrizs <aemielvin.loremia@gmail.com>
2016-05-08 17:51:53 +0800

    Remove some code parts not needed for networking

---
+++ Message.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+
+enum MessageType : uint8_t {
+	CONNECT = 0,	// client->server: string( desiredName )
+	CONFIRM,		// server->client: string( assignedID + " " + assignedName )
+	MAP_REQUEST,	// client->server: string( "" )
+	MAP_DATA,		// server->client: MapMessage{ height, width } + string( mapData )
+	READY,			// client->server: string( "" )
+	GAME_START,		// server->client: string( "" )
+	UPDATE_INPUT,	// client->server: UpdateInputMessage{  }
+	UPDATE_DATA,	// server->client: UpdateDataMessage{  }
+	GAME_FINISH		// server->client: string( "" )
+};
+
+struct MessageHeader {
+	MessageType type;
+	size_t size;
+};
+
+struct MapMessage {
+	size_t height;
+	size_t width;
+};
+
+struct UpdateDataMessage {
+	uint8_t ID;
+	float posX;
+	float posY;
+	int spriteDir;
+	int spriteAction;
+};
+
+struct UpdateInputMessage {
+	uint8_t ID;
+	
+	bool moveUP;
+	bool moveDown;
+	bool moveLeft;
+	bool moveRight;
+	
+	bool interact;
+	bool stun;
+	bool shift;
+};
